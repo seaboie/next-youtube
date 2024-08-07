f086a70858d941aaa0c0f5c665d5d7dbe22398bf
kritbovorn <kritbovorn@gmail.com>
2024-08-07 09:31:10 +0700

    create: NextResponseApiError()

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,20 +1,15 @@
 import connect from "@/app/lib/db";
 import User from "@/app/lib/modals/user";
+import { nextResponseApiError } from "@/app/utils/api/util-error";
 import { NextResponse } from "next/server"
 
 export const GET = async () => {
-
     try {
         await connect();
 
         const users = await User.find();
         return new NextResponse(JSON.stringify(users), {status: 200});
     } catch (err: unknown) {
-        console.error(`🔥 🔥 🔥 🔥 🔥 Oops !!! : Have error `, err);
-        if (err instanceof Error) {
-            return new NextResponse("Instance of : Error fetching users " + err.message, {status: 500});
-        } else {
-            return new NextResponse("Have error " + err);
-        }
+        return nextResponseApiError(err, "🔥 🔥 🔥 🔥 🔥 Oops !!! :  Error fetching users... ", 500);
     }
 }
---
+++ app/utils/api/util-error.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export const nextResponseApiError = (err: unknown, message: string, status: number) => {
+    const baseMessage = `Error during ${message}`;
+
+    let errorMessage: string;
+
+    if (err instanceof Error) {
+        console.error(`${baseMessage}\n`, err.message, `\nStack : `, err.stack);
+        errorMessage = `${baseMessage}\n ${err.message} \nStack : ${err.stack}`;
+    } else if (typeof err === 'string') {
+        console.error(`${baseMessage}\n `, err);
+        errorMessage = `${baseMessage}\n ${err}`;
+    } else if (typeof err === 'object' && err !== null) {
+        console.error(`${baseMessage}\n`, JSON.stringify(err, null, 2));
+        errorMessage = `${baseMessage}\n `, JSON.stringify(err, null, 2);
+    } else {
+        console.error(`${baseMessage}\n`, err);
+        errorMessage = `${baseMessage}\n`, err;
+    }
+
+    return new NextResponse(baseMessage + '\n' + errorMessage, {status: status});
+}
