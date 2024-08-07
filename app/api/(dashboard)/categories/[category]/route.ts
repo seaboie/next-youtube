6bb47e799d2d99f1f70bce2268bb5df2a4eb3454
kritbovorn <kritbovorn@gmail.com>
2024-08-07 22:59:10 +0700

    category: PATCH

---
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -0,0 +1,68 @@
+import connect from "@/app/lib/db";
+import Category from "@/app/lib/modals/category";
+import User from "@/app/lib/modals/user";
+import { nextResponseApiError } from "@/app/utils/api/util-error"
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export const PATCH = async (request: Request, context: {params: any}) => {
+    // Get categoryId
+    const categoryId = context.params.category;
+    
+    try {
+        const body = await request.json();
+        const { title } = body;
+
+        // Get userId
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get("userId");
+
+        if (!userId || !Types.ObjectId.isValid(userId))  {
+            return new NextResponse(
+                JSON.stringify({ message: " Invalid or missing userId"}),
+                {status: 400}
+            );
+        }
+
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+            return new NextResponse(  
+                JSON.stringify({ message: " Invalid or missing categoryId"}),
+                {status: 400}
+            );
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return new NextResponse(
+                JSON.stringify({ message: "User not found"}),
+                {status: 404}
+            );
+        }
+
+        const category = await Category.findOne({ _id: categoryId, user: userId});
+        
+        if (!category) {
+            return new NextResponse(  
+                JSON.stringify({ message: "Category not found"}),
+                {status: 404}
+            );
+        }
+
+        const updatedCategory = await Category.findByIdAndUpdate(  
+            categoryId,
+            { title },
+            { new: true }
+        );
+
+        return new NextResponse(  
+            JSON.stringify({ message: "Category is updated ", category: updatedCategory}),
+            {status: 200}
+        );
+        
+    } catch (err: unknown) {
+        return nextResponseApiError("Erro updating category", err, 500);
+    }
+}
