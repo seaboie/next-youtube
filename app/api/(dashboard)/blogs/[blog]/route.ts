6e3b473cbad91e90ee689a6933b298a5e3ea690c
kritbovorn <kritbovorn@gmail.com>
2024-08-09 18:23:18 +0700

    blog: PATCH()

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -1,9 +1,18 @@
 import connect from "@/app/lib/db";
 import Blog from "@/app/lib/modals/blog";
+import User from "@/app/lib/modals/user";
 import { nextResponseApiError } from "@/app/utils/api/util-error";
-import { checkUserCategoryFindById, checkUserIdCategoryIdBlogId } from "@/app/utils/check/check_user_category_userId_categoryId";
-import { EnumErrorMessageBlogs, EnumMessageBlogs } from "@/app/utils/enum/enum-message/enum_message";
+import {
+  checkUserCategoryFindById,
+  checkUserIdCategoryIdBlogId,
+} from "@/app/utils/check/check_user_category_userId_categoryId";
+import {
+  EnumErrorMessageBlogs,
+  EnumMessageBlogs,
+  EnumMessageUsers,
+} from "@/app/utils/enum/enum-message/enum_message";
 import { getValueOfDynamicRoute } from "@/app/utils/route/util-route";
+import { Types } from "mongoose";
 import { NextResponse } from "next/server";
 
 export const GET = async (request: Request, context: { params: any }) => {
@@ -15,48 +24,122 @@ export const GET = async (request: Request, context: { params: any }) => {
     const userId = searchParams.get("userId");
     const categoryId = searchParams.get("categoryId");
 
-    const checkErrorUserIdCategoryId = await checkUserIdCategoryIdBlogId(userId, categoryId, blogId);
+    const checkErrorUserIdCategoryId = await checkUserIdCategoryIdBlogId(
+      userId,
+      categoryId,
+      blogId
+    );
     if (checkErrorUserIdCategoryId) {
-        return new NextResponse(
-            JSON.stringify({message: checkErrorUserIdCategoryId.message}),
-            {status: checkErrorUserIdCategoryId.status}
-        );
+      return new NextResponse(
+        JSON.stringify({ message: checkErrorUserIdCategoryId.message }),
+        { status: checkErrorUserIdCategoryId.status }
+      );
     }
 
     await connect();
 
-    const checkErrorUserCategory = await checkUserCategoryFindById(userId, categoryId);
+    const checkErrorUserCategory = await checkUserCategoryFindById(
+      userId,
+      categoryId
+    );
     if (checkErrorUserCategory) {
-        return new NextResponse(
-            JSON.stringify({ message: checkErrorUserCategory.message}),
-            {status: checkErrorUserCategory.status}
-        );
+      return new NextResponse(
+        JSON.stringify({ message: checkErrorUserCategory.message }),
+        { status: checkErrorUserCategory.status }
+      );
     }
 
-    const blog = await Blog.findOne(
-        {
-            _id: blogId,
-            user: userId,
-            category: categoryId
-        }
-    );
+    const blog = await Blog.findOne({
+      _id: blogId,
+      user: userId,
+      category: categoryId,
+    });
 
     if (!blog) {
-        return new NextResponse(
-            JSON.stringify({message: EnumMessageBlogs.BLOG_NOT_FOUND}),
-            {status: 404}
-        );
+      return new NextResponse(
+        JSON.stringify({ message: EnumMessageBlogs.BLOG_NOT_FOUND }),
+        { status: 404 }
+      );
     }
 
-    return new NextResponse(
-        JSON.stringify({ blog }),
-        {status: 200}
-    );
-
-    
+    return new NextResponse(JSON.stringify({ blog }), { status: 200 });
   } catch (err) {
     return nextResponseApiError(
       EnumErrorMessageBlogs.ERROR_FECTHING_A_SPECIFIC_BLOG,
+      err,
+      500
+    );
+  }
+};
+
+export const PATCH = async (request: Request, context: { params: any }) => {
+  const blogId = context.params[Object.keys(context.params)[0]];
+
+  try {
+    const body = await request.json();
+    const { title, description } = body;
+
+    if (!title || !description) {
+      return new NextResponse(
+        JSON.stringify({ message: "Title and description are required" }),
+        { status: 400 }
+      );
+    }
+
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({
+          message: EnumMessageUsers.INVALID_OR_MISSING_USER_ID,
+        }),
+        { status: 400 }
+      );
+    }
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new NextResponse(
+        JSON.stringify({
+          message: EnumMessageBlogs.INVALID_OR_MISSING_BLOG_ID,
+        }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: EnumMessageUsers.USER_NOT_FOUND }),
+        { status: 404 }
+      );
+    }
+
+    const blog = await Blog.findOne({ _id: blogId, user: userId });
+    if (!blog) {
+      return new NextResponse(
+        JSON.stringify({ message: EnumMessageBlogs.BLOG_NOT_FOUND }),
+        { status: 404 }
+      );
+    }
+
+    const updateBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      { title, description },               // 
+      { new: true, runValidators: true }    // Must have
+    );
+
+    return new NextResponse(
+      JSON.stringify({
+        message: EnumMessageBlogs.BLOG_UPDATED,
+        blog: updateBlog,
+      }),
+      { status: 200 }
+    );
+  } catch (err: unknown) {
+    return nextResponseApiError(
+      EnumErrorMessageBlogs.ERROR_PATCH_A_SPECIFIC_BLOG,
       err,
       500
     );
--- app/utils/api/util-error.ts
+++ app/utils/api/util-error.ts
@@ -1,23 +1,29 @@
 import { NextResponse } from "next/server";
 
-export const nextResponseApiError = (message: string, err: unknown, status: number) => {
-    const baseMessage = `🔥 🔥 🔥 🔥 🔥  Oops !!! :   ${message}`;
+export const nextResponseApiError = (
+  message: string,
+  err: unknown,
+  status: number
+) => {
+  const baseMessage = `🔥 🔥 🔥 🔥 🔥  Oops !!! :   ${message}`;
 
-    let errorMessage: string;
+  let errorMessage: string;
 
-    if (err instanceof Error) {
-        console.error(`${baseMessage}\n`, err.message, `\nStack : `, err.stack);
-        errorMessage = `${baseMessage}\n${err.message} \nStack : ${err.stack}`;
-    } else if (typeof err === 'string') {
-        console.error(`${baseMessage}\n `, err);
-        errorMessage = `${baseMessage}\n ${err}`;
-    } else if (typeof err === 'object' && err !== null) {
-        console.error(`${baseMessage}\n`, JSON.stringify(err, null, 2));
-        errorMessage = `${baseMessage}\n `, JSON.stringify(err, null, 2);
-    } else {
-        console.error(`${baseMessage}\n`, err);
-        errorMessage = `${baseMessage}\n`, err;
-    }
+  if (err instanceof Error) {
+    // console.error(`${baseMessage}\n`, err.message, `\nStack : `, err.stack);
+    errorMessage = `${baseMessage}\n${err.message} \nStack : ${err.stack}`;
+  } else if (typeof err === "string") {
+    // console.error(`${baseMessage}\n `, err);
+    errorMessage = `${baseMessage}\n ${err}`;
+  } else if (typeof err === "object" && err !== null) {
+    // console.error(`${baseMessage}\n`, JSON.stringify(err, null, 2));
+    (errorMessage = `${baseMessage}\n `), JSON.stringify(err, null, 2);
+  } else {
+    // console.error(`${baseMessage}\n`, err);
+    (errorMessage = `${baseMessage}\n`), err;
+  }
 
-    return new NextResponse(baseMessage + '\n' + errorMessage, {status: status});
-}
+  return new NextResponse(baseMessage + "\n" + errorMessage, {
+    status: status,
+  });
+};
